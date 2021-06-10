6c4c74ffa6cf6472a9e1f87fef05ac0030a1f7af
Rajinikanth7 <74007661+Rajinikanth7@users.noreply.github.com>
2021-06-10 17:08:20 +0530

    Add files via upload

---
+++ Part-2/coursecontent/src/components/Course.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Total from './Total'
+import Content from './Content'
+
+
+const Course = ({courses}) =>
+  <div>
+    {courses.map(course =>
+      <div key={course.id}>
+        <Header course={course.name} />
+        <Content parts={course.parts}/>
+        <Total parts={course.parts}/>
+      </div>
+    )}
+  </div>
+
+export default Course
